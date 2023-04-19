492ea043d8c9151a05fac4381727dd60fefecc81
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-19 19:40:23 -0400

    pass in todo items from parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  items = [
+    {id: 'todo1', text: 'eat apples'},
+    {id: 'todo2', text: 'bake bread'},
+    {id: 'todo3', text: 'walk dog'},
+  ];  
+}
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
-
+  @Input() items: any[] = [];  
 }
